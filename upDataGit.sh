ab1039e2fe932cacb79d5a2b9929df32e2772281
Outsrkem <xhdascnf@126.com>
2019-10-13 15:44:54 +0800

    2019-10-13 15:44:54

--- upDataGit.sh
+++ upDataGit.sh
@@ -1,13 +1,19 @@
-#!/bin/bash -v
+#!/bin/bash
 # Sun Oct 13 15:42:19 CST 2019
 # 把本地文件推送到git 仓库
 export LANG=en_US.UTF-8
 SHHOME=$(cd `dirname $0`; pwd)
 cd $SHHOME
+echo --------------
 git add -A
+echo --------------
 git status
+echo --------------
 git commit -m "`date +%Y-%m-%d\ %H:%M:%S`"
+echo --------------
 git fetch
+echo --------------
 git merge origin/master
+echo --------------
 git push origin master
 
