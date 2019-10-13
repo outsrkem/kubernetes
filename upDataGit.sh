697cf859ed6d2a01acf05fefbccc355874218c59
Outsrkem <xhdascnf@126.com>
2019-10-13 15:46:07 +0800

    2019-10-13 15:46:07

--- upDataGit.sh
+++ upDataGit.sh
@@ -6,7 +6,7 @@ SHHOME=$(cd `dirname $0`; pwd)
 cd $SHHOME
 git add -A
 git status
-echo '-->git commit -m "`date +%Y-%m-%d\ %H:%M:%S`"'
+echo -e '----> git commit -m "`date +%Y-%m-%d\ %H:%M:%S`"'
 git commit -m "`date +%Y-%m-%d\ %H:%M:%S`"
 echo --------------
 git fetch
