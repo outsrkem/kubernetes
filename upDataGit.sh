f3b9a23e10acd9c7b0a6584e21e00de892a320fe
Outsrkem <xhdascnf@126.com>
2019-10-13 13:24:47 +0800

    更新说明

--- upDataGit.sh
+++ upDataGit.sh
@@ -3,7 +3,5 @@ git add .
 git commit -m "更新说明"
 git fetch
 git merge origin/master
-git merge
-git pull origin master --allow-unrelated-histories
 git push origin master
-git push origin HEAD:master
+
