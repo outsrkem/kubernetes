cee385bd359ed59a9afa0ea5a60d720a47bf31b1
Outsrkem <xhdascnf@126.com>
2020-08-01 12:30:31 +0800

    2020-08-01 12:30:31

--- upDataGit.sh
+++ upDataGit.sh
@@ -23,13 +23,13 @@ export LANG=en_US.UTF-8
 SHHOME=$(cd `dirname $0`; pwd)
 cd $SHHOME
 echo -e "\033[32m----> `pwd` \033[0m"
-git add -A
+git add .
 git status
 echo -e "\033[32m----> git commit -m "`date +%Y-%m-%d\ %H:%M:%S`"\033[0m"
 git commit -m "`date +%Y-%m-%d\ %H:%M:%S`"
 echo -e "\033[32m----> git fetch \033[0m"
-git fetch
-echo -e "\033[32m----> git merge origin/master\033[0m"
-git merge origin/master
-echo -e "\033[32m----> git push origin master\033[0m"
+#git fetch
+#echo -e "\033[32m----> git merge origin/master\033[0m"
+#git merge origin/master
+#echo -e "\033[32m----> git push origin master\033[0m"
 git push origin master
